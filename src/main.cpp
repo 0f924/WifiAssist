b880404a35d88f8a8dafb19cdfd856c6bc6cc985
lzjqsdd <lzj7179@163.com>
2016-07-18 19:27:41 +0800

    控制WifiAssist始终运行的是一个实例

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QCoreApplication>
+#include "QWindow"
+#include "singleapplication/singleapplication.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication::setApplicationName("{WifiAssist}");
+    QApplication::setOrganizationName("{lzjqsdd}");
+
+    //QApplication a(argc, argv);
+    SingleApplication a(argc,argv);
     a.setWindowIcon(QIcon("img/WifiAssit.ico"));
     MainWindow w;
     w.show();
